a97b20a203f4f21f4929691827ecb27619f6175f
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-01-08 22:45:43 +0530

    remove

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'myrdsdemo',
         'USER': 'mysuperuser',
         'PASSWORD': 'mysuperuser',
-        'HOST': 'myrdsdata.c1asac4y4rwk.ap-south-1.rds.amazonaws.com',
+        'HOST': '',
         'PORT': '5432',
     }
 }
@@ -150,11 +150,11 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
-# AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
-# AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
-# AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
+# AWS_ACCESS_KEY_ID = ''
+# AWS_SECRET_ACCESS_KEY = ''
+# AWS_STORAGE_BUCKET_NAME = 'backenddemo'
 # AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
+# AWS_S3_REGION_NAME = ''
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL =  None
 # AWS_S3_VERIFY = True
