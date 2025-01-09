507211cef750f75ab98112f2076df94861b4c7ac
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-01-09 13:41:47 +0530

    removed

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myrdsdemo',
+        'NAME': '',
         'USER': 'mysuperuser',
         'PASSWORD': 'mysuperuser',
         'HOST': '',
@@ -150,12 +150,3 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
-# AWS_ACCESS_KEY_ID = ''
-# AWS_SECRET_ACCESS_KEY = ''
-# AWS_STORAGE_BUCKET_NAME = 'backenddemo'
-# AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = ''
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  None
-# AWS_S3_VERIFY = True
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
